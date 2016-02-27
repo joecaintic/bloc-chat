58ef72c940f38cba7a645d5b25ebeb7fed23a56f
joecaintic <joecaintic@yahoo.com>
2016-02-27 18:17:51 -0500

    able to create new rooms

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,6 +2,12 @@
   function Room($firebaseArray) {
     var firebaseRef = new Firebase("https://blazing-fire-7433.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef.child('rooms'));
+   // var roomName="";  
+    
+   // this.addRoom = function() {  
+     //     rooms.$add({ $value: roomName });
+    //  }
+    
     return {
       all: rooms
     };
