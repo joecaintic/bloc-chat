86d1d69db7f7171224388c11f1d457ed9fd9c500
joecaintic <joecaintic@yahoo.com>
2016-03-15 00:18:09 -0400

    almost done need help

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,17 +2,25 @@
   function Room($firebaseArray) {
     var firebaseRef = new Firebase("https://blazing-fire-7433.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef.child('rooms'));
-   // var roomName="";  
-    
-   // this.addRoom = function() {  
-     //     rooms.$add({ $value: roomName });
-    //  }
+   // var msgsRef = new Firebase("https://blazing-fire-7433.firebaseio.com/messages"); 
+     // var msgs = $firebaseArray(firebaseRef.child('messages'));
     
     return {
-      all: rooms
-    };
-      
+      all: rooms,
+      //  create: function(newRoom) {
+        // Create method logic
+    //  },
+      getMessages: function(roomId) {
+      //  var rID=roomId;
+      //  console.log(rID);
+        var msgRef=firebaseRef.child('messages').orderByChild('roomId').equalTo(roomId);
 
+          return $firebaseArray(msgRef);
+            //.on('value', function(snapshot) {console.log(snapshot.val())});
+          // getMessages query logic
+      
+        }
+    }
   }
 
   angular
--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,21 +1,49 @@
 (function() {
-     function LandingCtrl(Room, $uibModal) {
-         var lc=this;
-         lc.title="Bloc Chat";
-         lc.rooms=Room.all;
-         lc.open=open;
+     function LandingCtrl(Room, $uibModal, $scope) {
+         this.title="Bloc Chat";
+         this.rooms=Room.all;
+         this.open=open;
+         this.roomName="Select a Room";
+         this.messages=[];
          
-         lc.animationsEnabled = true;
+         
+         this.setRoom = function (room) {
+		//    var roomId=room.$id;
+             this.roomName = room.$value;
+           this.roomVal=room.$id;
+             this.messages=Room.getMessages(this.roomVal);
+         };
+         
+   //      $scope.currentRoom={};
+         
+         
+      //   $rootScope.$on('SET_CURRENT_ROOM', function(event, ref){
+    //        var room = $scope.rooms[$scope.rooms.$indexFor(ref)]
+    //        $scope.setCurrentRoom(room);
+    //        });
+         
+        // console.log($scope);
+     //    $scope.setCurrentRoom=function(room) {
+    //         $scope.currentRoom=room;
+    //     };
+         
+        // lc.findRoom= function($index){
+        //     lc.rooms[$index] = $scope.room;
+         //};
+        // lc.selected="";
+         
+         this.animationsEnabled = true;
 
           function open() {
             var modalInstance = $uibModal.open({
-                animation: lc.animationsEnabled,
+                animation: this.animationsEnabled,
                 templateUrl: '/templates/newroom.html',
                 size: "sm",
                 controller: "NewRoomCtrl",
                 controllerAs: "newroom",
                });
              };
+         
      }
  
      angular
